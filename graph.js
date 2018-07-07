ce3e31921fe9f1ca4b95e96cbe5ade51b9fbf012
Drez3a <drezfabiola@gmail.com>
2018-07-07 01:17:36 -0300

    eventos concluídos / main feature: interação após simulação

--- graph.js
+++ graph.js
@@ -1,193 +1,233 @@
-var margin = {top: 30, right: 20, bottom: 20, left: 30}; 
-var width = 820;
+var margin = {top: 10, left: 30, bottom: 10, right: 20}; 
+var width = 800;
 var height = 520;
 var drawWidth = width - margin.left - margin.right; 
 var drawHeight = height - margin.top - margin.bottom; 
 
+// zoom settings
+var scaleFactor = 0.25;
+var translateFactor = [1200,700];
+var zoomCentered = null;
+var zoomLevel = 0.5;
+
 d3.select("body")
-	.append("g")
-  .append("h1")
-  .text("Github")
-  .style("fill", "#039BE5");
+  .append("g")
+  .append("h2")
+  .text("Rede Github Recife");
 
 d3.select("body").on("contextmenu",d=>{
   d3.event.preventDefault()
     return false; });
-  
+
 var svg = d3.select("body")
   .append("svg")            
   .attr("width", width)
   .attr("height", height)
-  .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-  .append("g")
-  // falta o pan
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
-// d3.csv("data/edges.csv", function(error, edges){	
-// 	if(error) { console.log(error); }		
-// 	buildGraph(edges);	
-// });
-
-
+// dataset
 d3.csv("data/edges.csv", function(error, edges){
-	d3.csv("data/gephi/node_data.csv", function(error, csv){
-		if(error) { console.log(error); }
-
-		var data = crossfilter(csv);
-		console.log(data.size());
-		buildGraph(edges, data);
-	});  
+  d3.csv("data/gephi/node_data.csv", function(error, csv){
+  	if(error) { console.log(error); }
+		var data_cf = crossfilter(csv);
+		buildGraph(edges, data_cf);
+  });  
 });
 
-function buildGraph(edges,data){
 
-//function buildGraph(edges){
-	var nodes = {};
-	var links = edges;
+function buildGraph(edges,data_cf){
+  var links = edges;
+  var nodesConected = {};
+  var nodes = []; 
 
-	var cScale = d3.scaleLinear().domain([1,80]).range([5, 20]); 
+  var node_data_dm = data_cf.dimension(function(d,i){ 
+    return d["User"];  }); 
+  var node_data = node_data_dm.bottom(Infinity);
 
-	// zoom settings
-	var zoomCentered = null;
-	var zoomLevel = 2;
+  var interval = d3.extent(node_data,d=>parseInt(d.In_Degree));
+  var rScale = d3.scaleLinear().domain(interval).range([5, 25]); 
 
-	links.forEach(function(link) {	
-		link.source = nodes[link.source] ||
-			(nodes[link.source] = {user: link.source });			
-		link.target = nodes[link.target] ||
-			(nodes[link.target] = {user: link.target });
-	});
+  var qtd = {};
+    node_data.map(e=>{ 
+    return qtd[e.Modularity_Class] = qtd[e.Modularity_Class] + 1 || 1  });
+  var colorScale = d3.scaleSequential(d3.interpolateCool).domain([-3, Object.keys(qtd).length]); // interpolateRainbow interpolateCool
 
-	var link = svg.append("g")
-		.attr("class", "link")
-		.selectAll(".link")
-		.data(links)
-		.enter()
-		.append("line")
-		.style("stroke", "#c5a688")
-		.style("stroke-width", "2px")
-		.style("stroke-opacity", 0.7);
+  var initialTransform =  d3.zoomIdentity.scale(1.5).translate(-140,-60);
 
-	var node = svg.append("g") 
-		.selectAll(".node")
-		.data(d3.values(nodes))
-		.enter()
-		.append("g")
-		.attr("class", "node")
+  links.forEach(function(link) {	
+	  link.source = nodesConected[link.source] || 
+      (nodesConected[link.source] = {User: link.source });			
+	  link.target = nodesConected[link.target] ||
+	    (nodesConected[link.target] = {User: link.target });
+  });
 
-	var circle = node.append("circle")
-		.style("fill", "#5ac5d1")
-		.style("stroke", "white")	
-		//.attr("stroke-width", "1.5px")	
-		.attr("r", 
-			d=>cScale(3.14*links.filter(e=>{  // boa proporcao entre as areas
-			return e.source.user == d.user || e.target.user == d.user}).length
-		));  
-	
-	// eventos 
-	node.attr("cursor", "zoom-in")
-		.on("click", clicked)  // contem zoom behavior
-		.call(d3.drag()
-			.on("start", ondragstart)
-			.on("drag", ondrag)
-			.on("end", ondragend))
-		.call(d3.zoom()  // pensando em usar apenas o pan do zoom behavior (e o zoom por nodes)
-		  	.scaleExtent([-1, 1])
-		  	.on("zoom", function () {  // demo
-		  		svg.attr("transform", d3.event.transform) })
-		 );
+  // var nodes = d3.values(nodesConected); 
+  // ordenando
+  Object.keys(nodesConected)
+    .sort()
+    .forEach(function(v, i) {      
+      nodes[i] = nodesConected[v] 
+  });
+ 
+  var graph = svg.append("g")
+		.attr("class", "complexNetwork")
+    .attr("transform", "scale(" + scaleFactor + ")translate(" + translateFactor[0] + "," + translateFactor[1] + ")");  
 
-	var labels = node.append("g")
-	 	.append("text")
-	    .attr("font-family", "Verdana")
-	    .attr("font-size", 10)
-		.attr("text-anchor", "middle") 
-	    .text(function(d) { return d.user; }); 
+  var link = graph.append("g")
+    .attr("class", "links")
+    .selectAll("links")
+    .data(links)
+    .enter()
+    .append("line")
+    .style("stroke", "#c5a688")
+    .style("stroke-width", "2px")
+    .style("stroke-opacity", 0.7);
 
-	var force_dirGraph = d3.forceSimulation()			
-		.force("link", d3.forceLink(links)) //.id(function(d) { return d.id; }))
-		.force("charge", d3.forceManyBody())			
-		.force("center", d3.forceCenter(drawWidth/2, drawHeight/2))
-		//.force("radial",d3.forceRadial().radius(1.5))  // oculta caminhos indo para os nos de grau baixo, porem destaca clusters (a confirmar)
-		.force("collision", d3.forceCollide().radius(//25));
-			d=>(1.5*links.filter(e=>{
-			return e.source.user == d.user || e.target.user == d.user}).length)) );
-	    
-	force_dirGraph.nodes(d3.values(nodes))			
-		.on("tick", ticked);	
+  var node = graph.append("g")
+    .attr("class", "nodes") 
+    .selectAll("nodes")
+    .data(nodes)
+    .enter()
+    .append("g")
+    .attr("cursor", "cell");
 
-	function clicked(d) {
-		var x;
-		var y;
-		var level;
+  var circle = node.append("circle")
+    .attr("r", (d,i)=>rScale(
+      3*parseInt(node_data[i].In_Degree)  ))
+    .attr("fill", (d,i)=>colorScale(
+      parseInt(node_data[i].Modularity_Class)  ))
+    .attr("stroke", "white")   
+    .attr("stroke-width", "2px");    
 
-		if(!d3.event.ctrlKey){
-			if (zoomCentered !== d) {		
-				x = d.x; 
-				y = d.y; 
-				level = zoomLevel;
-				zoomCentered = d;		
-			} else {
-				x = width/2;
-				y = height/2;
-				level = 1;
-				zoomCentered = null;
-			}
-			svg.transition()
-			  .duration(1000)
-			  .ease(d3.easeCubicOut)
-			  .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + level + ")translate(" + -x + "," + -y + ")");
-		} else {  // o usuario libera o nó que fixou
-		    d.fx = null;
-		    d.fy = null;
-		}			
+  var label = node.append("text")
+    .attr("font-family", "Verdana")
+    .attr("font-size", 10)
+    .attr("text-anchor", "middle") 
+    .text(function(d) { return d.User; }); 
+
+  var forceDirGraph = d3.forceSimulation()
+	  .nodes(nodes)
+    .force("links", d3.forceLink(links));
+     
+  forceDirGraph
+    .force("charge", d3.forceManyBody())           
+    .force("center", d3.forceCenter(drawWidth / 2, drawHeight / 2))
+    //.force("radial",d3.forceRadial().radius(1.5)) 
+    .force("collision", d3.forceCollide().radius((d,i)=>
+      (3*parseInt(node_data[i].In_Degree) + 5/*normalizacao*/) ));
+
+  // permite a continuacao drag_handler após o fim da simulacao
+  forceDirGraph.on("tick", tick_actions)
+    .on("end", function() {
+      node.each(function(d) {
+    	  d.fx = d.x;
+        d.fy = d.y; })  });
+  
+  var zoom_handler = d3.zoom()
+    .on("zoom", zoom_actions);
+
+  zoom_handler(svg); 
+
+  node.on("click", clicked); 
+
+  var drag_handler = d3.drag()
+    .on("start", drag_start)
+    .on("drag", drag_drag)
+    .on("end", drag_end); 
+
+  drag_handler(node); 
+ 
+  // navegacao por node 
+  // liberacao do node
+  function clicked(d,i) {
+    var x;
+    var y;
+    var level;
+
+    if(!d3.event.ctrlKey){
+      if (zoomCentered !== d) {       
+        x = d.x; 
+        y = d.y; 
+        level = 3*zoomLevel; 
+        zoomCentered = d; 
+
+        update_zoom();
+        update_nodes();  // disable node
+        d3.select(this).select("circle").style("fill", "cyan");
+ 				
+      } else {
+        x = width/2;
+        y = height/2;
+        level = 0.8*zoomLevel;
+        zoomCentered = null;
+        d3.select(this).select("circle").style("fill", colorScale(
+          parseInt(node_data[i].Modularity_Class)  ));
+    	}
+     	graph.transition()
+        .duration(1000)
+        .ease(d3.easeCubicOut)
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + level + ")translate(" + -x + "," + -y + ")");	
+
+    // o usuario libera o nó que fixou    
+    } else {  
+      d.fx = null;
+      d.fy = null;
+      d3.select(this).select("circle").style("fill", colorScale(
+          parseInt(node_data[i].Modularity_Class)  ));
+    }  
+  }
+  
+  function zoom_actions(){
+		var transform = d3.event.transform.scale(scaleFactor).translate(translateFactor[0],translateFactor[1]); 
+		graph.attr("transform", transform);
+		// console.log(transform);
+  } 
+
+  function update_zoom() {  
+  	svg.transition()
+      .duration(1000)
+      .call(zoom_handler.transform, initialTransform);
 	}
 
-	function ondragstart(d) {
-		if (!d3.event.active) force_dirGraph.alphaTarget(0.3).restart();
-		  d.fx = d.x;
-		  d.fy = d.y;
-	}
+  function update_nodes() {
+    upNodes = graph.selectAll("circle")
+      .style("fill", (d,i)=>colorScale(
+      parseInt(node_data[i].Modularity_Class)  ))
+  }
 
-	function ondrag(d) {		
+  function drag_start (d) {
+    if (!d3.event.active) forceDirGraph.alphaTarget(0.3).restart();    
+      d.fx = d.x;
+      d.fy = d.y;
+  }
+   
+  function drag_drag(d) {
 		d.fx = d3.event.x;
-		d.fy = d3.event.y;	  
-	}
+		d.fy = d3.event.y;
+  }
 
 	// o usuario fixa a posicao do nó
-	function ondragend(d) {
-		if (!d3.event.active) force_dirGraph.alphaTarget(0);
-		  d.fx = d.x;
-		  d.fy = d.y;
-	}	
+  function drag_end(d) {
+    if (!d3.event.active) forceDirGraph.alphaTarget(0);
+    	d.fx = d.x;
+      d.fy = d.y;
+    d3.select(this).select("circle").style("fill", "cyan"); 
+  }
 
-	function ticked() {	
+	function tick_actions() {	
+    link.attr("x1", function(d) { return d.source.x; })
+      .attr("y1", function(d) { return d.source.y; })
+      .attr("x2", function(d) { return d.target.x; })
+      .attr("y2", function(d) { return d.target.y; });
+        
+    circle.attr("cx", function(d) { return d.x; })
+      .attr("cy", function(d) { return d.y; });
+            
+    label.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+  }
 
-	link.attr("x1", function(d) { return d.source.x; })
-		.attr("y1", function(d) { return d.source.y; })
-		.attr("x2", function(d) { return d.target.x; })
-		.attr("y2", function(d) { return d.target.y; });
-		
-	circle.attr("cx", function(d) { return d.x; })
-		.attr("cy", function(d) { return d.y; });
-			
-	labels.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
-	}
+// heatmap(node_data);
 
-
-// testes... __________________
-
-// var xScale = d3.scaleLinear().domain([]).range([0, drawWidth]);
-// var yScale = d3.scaleLinear().domain([]).range([0, drawHeight]);
-
-// function ticked() {	
-// 	node.attr("cx", function(d) { return xScale(d.x); })
-// 		.attr("cy", function(d) { return yScale(d.y); });
-			
-// 	link.attr("x1", function(d) { return xScale(d.source.x); })
-// 		.attr("y1", function(d) { return yScale(d.source.y); })
-// 		.attr("x2", function(d) { return xScale(d.target.x); })
-// 		.attr("y2", function(d) { return yScale(d.target.y); });
-// 	}
-		
-} // end
+console.log("node_data:", node_data);    
+}
